045ca5f09fcc6417fb5fd5e42fdeedddbf68841c
Vitaly <v31337@gmail.com>
2022-04-21 16:25:53 +0300

    Disable some minor code for Android and *BSD

--- core/unixptyprocess.cpp
+++ core/unixptyprocess.cpp
@@ -3,7 +3,7 @@
 
 #include <termios.h>
 #include <errno.h>
-#ifndef __ANDROID__
+#if !defined(Q_OS_ANDROID) && !defined(Q_OS_FREEBSD)
 #include <utmpx.h>
 #endif
 #include <fcntl.h>
@@ -333,7 +333,7 @@ void ShellProcess::setupChildProcess()
     ioctl(m_handleSlave, TIOCSCTTY, 0);
     tcsetpgrp(m_handleSlave, sid);
 
-#ifndef __ANDROID__
+#if !defined(Q_OS_ANDROID) && !defined(Q_OS_FREEBSD)
     // on Android imposible to put record to the 'utmp' file
     struct utmpx utmpxInfo;
     memset(&utmpxInfo, 0, sizeof(utmpxInfo));
